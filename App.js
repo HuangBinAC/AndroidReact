b51a5424391f62dc3d2cf221e749a2c276216ff4
Bin Huang <v-bihuan@microsoft.com>
2018-01-31 16:20:48 +0800

    upload

--- App.js
+++ App.js
@@ -9,8 +9,13 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+// Import App Center Crashes at the top of the file.
+import Crashes from 'appcenter-crashes';
+import Push from 'appcenter-push';
+import { AppState, Alert } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -19,7 +24,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -29,6 +34,11 @@ export default class App extends Component<{}> {
         <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
+        <Button
+            title="crash" 
+            onPress={onCrash}
+        >    
+        </Button>
         <Text style={styles.instructions}>
           {instructions}
         </Text>
@@ -36,6 +46,53 @@ export default class App extends Component<{}> {
     );
   }
 }
+function onCrash(){
+  throw new Error("This is a test JavaScript Crash!"); 
+}
+import Crashes, { ErrorAttachmentLog } from 'appcenter-crashes';
+
+Crashes.setListener({
+    getErrorAttachments(report) {
+        const textAttachment = ErrorAttachmentLog.attachmentWithText('Hello text attachment!', 'hello.txt');
+        const binaryAttachment = ErrorAttachmentLog.attachmentWithBinary(`${imageAsBase64string}`, 'logo.png', 'image/png');
+        return [textAttachment, binaryAttachment];
+    }
+
+    // Other callbacks must also be defined at the same time if used.
+    // Default values are used if a method with return parameter is not defined.
+});
+
+class MyApp extends Component {
+}
+
+Push.setListener({
+  onPushNotificationReceived: function (pushNotification) {
+    let message = pushNotification.message;
+    let title = pushNotification.title;
+
+    if (message === null || message === undefined) {
+      // Android messages received in the background don't include a message. On Android, that fact can be used to
+      // check if the message was received in the background or foreground. For iOS the message is always present.
+      title = 'Android background';
+      message = '<empty>';
+    }
+
+    // Custom name/value pairs set in the App Center web portal are in customProperties
+    if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+      message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+    }
+
+    if (AppState.currentState === 'active') {
+      Alert.alert(title, message);
+    }
+    else {
+      // Sometimes the push callback is received shortly before the app is fully active in the foreground.
+      // In this case you'll want to save off the notification info and wait until the app is fully shown
+      // in the foreground before displaying any UI. You could use AppState.addEventListener to be notified
+      // when the app is fully in the foreground.
+    }
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
--- android/app/src/main/java/com/androidreact/MainApplication.java
+++ android/app/src/main/java/com/androidreact/MainApplication.java
@@ -3,6 +3,8 @@ package com.androidreact;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.push.Push;
+import com.microsoft.appcenter.reactnative.push.AppCenterReactNativePushPackage;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativePushPackage(MainApplication.this),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appcenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appcenterAnalytics_whenToEnableAnalytics)),
             new AppCenterReactNativePackage(MainApplication.this)
@@ -46,6 +49,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    Push.setSenderId("960476865317");
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
